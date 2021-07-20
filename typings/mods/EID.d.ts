5dbec88dd133027b1096c5ec26bc2c43272285b7
Somdudewillson <somdudewilson@gmail.com>
2021-07-20 13:46:31 -0700

    * */ -> */

--- typings/mods/EID.d.ts
+++ typings/mods/EID.d.ts
@@ -77,7 +77,7 @@ declare class ExternalItemDescriptions {
    * When type = entity, targetIdentifier must be in the format "ID.Variant.subtype". for any other type, it can just be the id
    *
    * EXAMPLE: `EID.assignTransformation("collectible", 1, "My Transformation")`
-   * */
+   */
   assignTransformation(
     targetType: string,
     targetIdentifier: string | int,
@@ -91,7 +91,7 @@ declare class ExternalItemDescriptions {
    * When type = entity, targetIdentifier must be in the format "ID.Variant.subtype". for any other type, it can just be the id
    *
    * EXAMPLE: `EID.removeTransformation("collectible", 1, "My Transformation")`
-   * */
+   */
   removeTransformation(
     targetType: string,
     targetIdentifier: string | int,
@@ -108,7 +108,7 @@ declare class ExternalItemDescriptions {
   /** Adds a description for a an Entity. Optional parameters: language
    *
    * When subtype is -1 or null, it will affect all subtypes of that entity
-   * */
+   */
   addEntity(
     id: int,
     variant: int,
@@ -125,7 +125,7 @@ declare class ExternalItemDescriptions {
    * Setting "animationFrame" to -1 will play the animation. The spriteObject needs to be of class Sprite() and have an .anm2 loaded
    *
    * Default values: leftOffset= -1 , topOffset = 0
-   * */
+   */
   addIcon(
     shortcut: string,
     animationName: string,
@@ -142,7 +142,7 @@ declare class ExternalItemDescriptions {
    * Shortcuts are case Sensitive! Shortcuts can be overridden with this function to allow for full control over everything
    *
    * Define a callback to let it be called when interpreting the color-markup. define a `KColor` otherwise for a simple color change
-   * */
+   */
   addColor(
     shortcut: string,
     kColor: KColor,
@@ -152,7 +152,7 @@ declare class ExternalItemDescriptions {
   /**
    * Overrides all potentially displayed texts and permanently displays the given texts. Can be turned off again using
    * {@link EID.hidePermanentText EID:hidePermanentText()}
-   * */
+   */
   displayPermanentText(descriptionObject: EIDDescriptionObj): void;
   /** Hides permanently displayed text objects if they exist. */
   hidePermanentText(): void;
@@ -176,19 +176,19 @@ declare class ExternalItemDescriptions {
    * Adds a text position modifier Vector, which will be applied to the text position variable
    *
    * Useful to add small offsets. For example: for schoolbag HUD
-   * */
+   */
   addTextPosModifier(identifier: string, modifierVector: Vector): void;
   /**
    * Removes a text position modifier Vector
    *
    * Useful to remove small offsets. For example: for schoolbag HUD
-   * */
+   */
   removeTextPosModifier(identifier: string): void;
   /**
    * Changes the initial position of all eid descriptions
    *
    * Useful to totally alter and override the current initial Overlay position
-   * */
+   */
   alterTextPos(newPosVector: Vector): void;
 
   /** returns the entity that is currently described. returns last described entity if currently not displaying text */
@@ -201,7 +201,7 @@ declare class ExternalItemDescriptions {
    * returns the description object of the specified entity
    *
    * falls back to english if the objID isn't available
-   * */
+   */
   getDescriptionObj(Type: int, Variant: int, SubType: int): EIDDescriptionObj;
   /** returns description Object from the legacy mod descriptions if they exist */
   getLegacyModDescription(
@@ -213,13 +213,13 @@ declare class ExternalItemDescriptions {
    * returns the specified object table in the current language.
    *
    * falls back to english if it doesn't exist
-   * */
+   */
   getDescriptionEntry(objTable: string, objID?: string): EIDDescriptionObj;
   /**
    * returns the description data table in the current language related to a given id, variant and subtype
    *
    * falls back to english if it doesn't exist
-   * */
+   */
   getDescriptionData(Type: int, Variant: int, SubType: int): EIDDescriptionObj;
 
   /** Returns an adjusted SubType id for special cases like Horse Pills and Golden Trinkets */
@@ -228,14 +228,14 @@ declare class ExternalItemDescriptions {
    * Get the transformation uniqueName / ID of a given entity
    *
    * Example: EID:getTransformation(5,100,34)  will return "12" which is the id for Bookworm
-   * */
+   */
   getTransformation(Type: int, Variant: int, SubType: int): string;
 
   /**
    * Get the name of the given transformation by its uniqueName / ID
    *
    * (Note: this function might be broken)
-   * */
+   */
   getTransformationName(id: string): string;
   /** tries to get the ingame name of an item based on its ID */
   getObjectName(Type: int, Variant: int, SubType: int): string;
@@ -253,18 +253,18 @@ declare class ExternalItemDescriptions {
   /**
    * Generates a string with the defined pixel-length using a custom 1px wide character
    * This will only work for this specific custom font
-   * */
+   */
   generatePlaceholderString(length: int): string;
   /**
    * Returns the `EIDInlineIcon` object of a given Iconstring
    *
    * can be used to validate an iconstring
-   * */
+   */
   getIcon(str: string): EIDInlineIcon;
   /**
    * Tries to read special markup used to generate icons for all Collectibles/Trinkets and the default Cards/Pills
    * @returns an `EIDInlineIcon` Object or `null` if no parsing was possible
-   * */
+   */
   createItemIconObject(str: string): EIDInlineIcon | null;
   /** Returns the icon for a given transformation name or ID */
   getTransformationIcon(str: string): EIDInlineIcon;
@@ -277,7 +277,7 @@ declare class ExternalItemDescriptions {
    *
    * - the string without the placeholders but with an accurate space between lines.
    * - an array of tables containing each Inline Sprite and the preceding text width
-   * */
+   */
   filterIconMarkup(
     text: string,
     textPosX?: int,
@@ -288,7 +288,7 @@ declare class ExternalItemDescriptions {
    * {@link EID.filterIconMarkup EID:filterIconMarkup()} function
    *
    * Table entry format: {`EIDInlineIcon` Object, Width of text preceding the icon}
-   * */
+   */
   renderInlineIcons(
     spriteTable: Array<[EIDInlineIcon, int]>,
     posX: int,
@@ -302,13 +302,13 @@ declare class ExternalItemDescriptions {
    * Gets a `KColor` from a Markup-string (example Input: "{{ColorText}}")
    *
    * @returns the `KColor` object and a `boolean` value indicating if the given string was a color markup or not
-   * */
+   */
   getColor(str: string, baseKColor: KColor): LuaMultiReturn<[KColor, boolean]>;
   /**
    * Filters a given string and looks for `KColor` markup. Splits the text into subsections limited by them.
    *
    * @returns: Table of subsections of the text, their respective `KColor`, and the width of the subsection
-   * */
+   */
   filterColorMarkup(
     text: string,
     baseKColor: KColor,
@@ -317,13 +317,13 @@ declare class ExternalItemDescriptions {
   /**
    * Fits a given string to a specific width
    * @returns the string as a table of lines
-   * */
+   */
   fitTextToWidth(str: string, textboxWidth: number): string[];
   /**
    * Renders a given string using the EID Custom font. This will also apply any markup and render icons
    * Needs to be called in a render Callback
    * @returns the last used `KColor`
-   * */
+   */
   renderString(
     str: string,
     position: Vector,
@@ -336,7 +336,7 @@ declare class ExternalItemDescriptions {
    * Used for altering descriptions. Example: Spindown dice, Tarot Cloth,...
    * @param condition function that returns `true` if `callback` should be called on the given description string
    * @param callback function that returns a modified version of the given description string
-   * */
+   */
   addDescriptionModifier(
     modifierName: string,
     condition: (testDescription: string) => boolean,
@@ -345,7 +345,7 @@ declare class ExternalItemDescriptions {
   /**
    * Removes a Description object modifier
    * Used for altering descriptions. Example: Spindown dice, Tarot Cloth,...
-   * */
+   */
   removeDescriptionModifier(modifierName: string): void;
 
   /** Interpolates between 2 KColors with a given fraction. */
@@ -361,7 +361,7 @@ declare class ExternalItemDescriptions {
    * Result: `"{{Crafting8}}{{Crafting7}}{{Crafting6}}{{Crafting5}}{{Crafting4}}{{Crafting3}}{{Crafting2}}{{Crafting1}}"`
    *
    * Prefer {@link EID.tableToCraftingIconsMerged tableToCraftingIconsMerged()}, due to improved render performance.
-   * */
+   */
   tableToCraftingIconsFull(craftTable: int[]): string;
   /**
    * Converts a given table into a string containing the crafting icons of the table, which are also grouped to reduce render lag
@@ -369,7 +369,7 @@ declare class ExternalItemDescriptions {
    * Example input: `{1,1,1,2,2,3,3,3}`
    *
    * Result: `"3{{Crafting3}}2{{Crafting2}}3{{Crafting1}}"`
-   * */
+   */
   tableToCraftingIconsMerged(craftTable: int[]): string;
 
   /** Gets the size of the screen */
